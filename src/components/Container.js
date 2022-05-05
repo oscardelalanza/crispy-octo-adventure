c6abd6d1f1bda732cf7b00e8774aad6cc5327ed3
Oscar De La Lanza <oscardelalanza@gmail.com>
2022-05-04 22:22:47 -0700

    refactor

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 const Container = styled.div`
   width: ${props => props.width || '100'}%;
   padding: 0;
-  margin-top: 0;
-  margin-bottom: 0;
   margin-right: ${props => props.mr || 0}px;
   margin-left: ${props => props.ml || 0}px;
 `
